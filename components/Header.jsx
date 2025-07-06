4fa363da299868548d1f35735461907afcf4d15e
Md. Muhaimin Islam Tanvir <tanvir.cse05@gmail.com>
2025-07-06 23:12:56 +0600

    sidebar added

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ import Sidebar from "./Sidebar";
 import Search from "./Search";
 const Header = ({docs}) => {
   return (
-    <header classNameName="fixed inset-y-0 left-0 z-40 contents w-72 overflow-y-auto border-r border-zinc-900/10 px-6 pb-8 pt-4 dark:border-white/10 lg:block xl:w-80">
+    <header className="fixed inset-y-0 left-0 z-40 contents w-72 overflow-y-auto border-r border-zinc-900/10 px-6 pb-8 pt-4 dark:border-white/10 lg:block xl:w-80">
       <Logo />
       <div
         className="fixed inset-x-0 top-0 z-50 bg-white bg-white/[var(--bg-opacity-light)] px-4 backdrop-blur-sm transition dark:bg-[#17181C] dark:backdrop-blur sm:px-6 lg:left-72 lg:z-30 lg:px-8 xl:left-80"
