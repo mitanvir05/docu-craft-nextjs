b12906a6ccfa5d2a657708a8d73ff98460cf53ea
Md. Muhaimin Islam Tanvir <tanvir.cse05@gmail.com>
2025-07-08 00:09:04 +0600

    done

--- components/ContentDisplay.jsx
+++ components/ContentDisplay.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import { getDocumentContent } from "@/lib/doc";
+import Link from "next/link";
+import Tag from "./Tag";
+const ContentDisplay = async ({id}) => {
+  const documentContent = await getDocumentContent(id);
 
-const ContentDisplay = ({id}) => {
+  //console.log(documentContent);
+
   return (
-    <div>
-      ContentDisplay
-    </div>
+    <article className="prose dark:prose-invert">
+      <h1>{documentContent.title}</h1>
+      <div>
+        <span>Published On: {documentContent.date}</span> by {" "}
+        <Link href={`/author/${documentContent.author}`}>
+          {documentContent.author}
+        </Link> {" "}
+        under the{" "}
+        <Link href={`/categories/${documentContent.category}`}>
+          {documentContent.category}
+        </Link>{" "}
+        category.
+      </div>
+      <div>
+        {documentContent.tags &&
+          documentContent.tags.map((tag) => <Tag key={tag} tag={tag} />)}
+      </div>
+      <div
+        className="lead"
+        dangerouslySetInnerHTML={{__html: documentContent.contentHtml}} />
+    </article>
   )
 }
 
