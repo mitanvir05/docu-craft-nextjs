13629c480d54d6c81cbbdf2b7a85bf8fdad2127c
Md. Muhaimin Islam Tanvir <tanvir.cse05@gmail.com>
2025-07-09 14:45:06 +0600

    tag name added

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,14 +1,61 @@
+"use client";
 import Link from "next/link";
-
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+import {
+  getDocumentsByAuthor,
+  getDocumentsByCategory,
+  getDocumentsByTag,
+} from "./utils/doc-util";
 const Sidebar = ({ docs }) => {
-  const roots = docs.filter((doc) => !doc.parent);
- // console.log({ roots });
+  const pathname = usePathname();
+  const [rootNodes, setRootNodes] = useState([]);
+  const [nonRootNodesGrouped, setNonRootNodesGrouped] = useState({});
 
-  const nonRoots = Object.groupBy(
-    docs.filter((doc) => doc.parent),
-    ({ parent }) => parent
-  );
-  //console.log({ nonRoots });
+  useEffect(() => {
+    let mathchedDocs = docs;
+    if (pathname.includes("/tags")) {
+      const tag = pathname.split("/")[2];
+      mathchedDocs = getDocumentsByTag(docs, tag);
+    } else if (pathname.includes("/authors")) {
+      const author = pathname.split("/")[2];
+      mathchedDocs = getDocumentsByAuthor(docs, author);
+    } else if (pathname.includes("/categories")) {
+      const category = pathname.split("/")[2];
+      mathchedDocs = getDocumentsByCategory(docs, category);
+    }
+
+    const roots = mathchedDocs.filter((doc) => !doc.parent);
+    // console.log({ roots });
+
+    const nonRoots = Object.groupBy(
+      mathchedDocs.filter((doc) => doc.parent),
+      ({ parent }) => parent
+    );
+    //console.log({ nonRoots });
+
+   const nonRootsKeys = Reflect.ownKeys(nonRoots);
+        nonRootsKeys.forEach(key => {
+            const foundInRoots = roots.find((root) => root.id === key);
+            if(!foundInRoots) {
+                const foundInDocs = docs.find((doc) => doc.id === key);
+                roots.push(foundInDocs);
+            }
+        });
+
+        roots.sort((a, b) => {
+            if (a.order < b.order) {
+              return -1;
+            }
+            if (a.order > b.order) {
+              return 1;
+            }
+            return 0;
+          });
+
+    setRootNodes([...roots]);
+    setNonRootNodesGrouped({ ...nonRoots });
+  }, [pathname]);
 
   return (
     <nav className="lg:block my-10">
@@ -18,27 +65,29 @@ const Sidebar = ({ docs }) => {
           <div className="absolute inset-y-0 left-2 w-px bg-zinc-900/10 dark:bg-white/5"></div>
           <div className="absolute left-2 h-6 w-px bg-emerald-500"></div>
           <ul role="list" className="border-l border-transparent">
-            {roots.map((rootNode) => (
-              <li key={rootNode.id} className="relative">
+            {rootNodes.map((rootNode) => (
+              <li key={rootNode?.id} className="relative">
                 <Link
                   aria-current="page"
                   className="flex justify-between gap-2 py-1 pl-4 pr-3 text-sm text-zinc-900 transition dark:text-white"
-                  href={`/docs/${rootNode.id}`}
+                  href={`/docs/${rootNode?.id}`}
                 >
-                  <span className="truncate">{rootNode.title}</span>
+                  <span className="truncate">{rootNode?.title}</span>
                 </Link>
-                {nonRoots[rootNode.id.replace(/\.$/, "")] && (
+                {nonRootNodesGrouped[rootNode?.id.replace(/\.$/, "")] && (
                   <ul role="list" className="border-l border-transparent">
-                    {nonRoots[rootNode.id.replace(/\.$/, "")].map((subRoot) => (
-                      <li key={subRoot.id}>
-                        <Link
-                          className="flex justify-between gap-2 py-1 pl-7 pr-3 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
-                          href={`/docs/${rootNode.id}/${subRoot.id}`}
-                        >
-                          <span className="truncate">{subRoot.title}</span>
-                        </Link>
-                      </li>
-                    ))}
+                    {nonRootNodesGrouped[rootNode.id.replace(/\.$/, "")].map(
+                      (subRoot) => (
+                        <li key={subRoot.id}>
+                          <Link
+                            className="flex justify-between gap-2 py-1 pl-7 pr-3 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
+                            href={`/docs/${rootNode.id}/${subRoot.id}`}
+                          >
+                            <span className="truncate">{subRoot.title}</span>
+                          </Link>
+                        </li>
+                      )
+                    )}
                   </ul>
                 )}
               </li>
